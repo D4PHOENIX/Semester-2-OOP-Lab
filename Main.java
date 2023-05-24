ae055e4fa20942939908a8f1675ed1f9699ccee5
Daud Noman <daudnoman@hotmail.com>
2023-05-24 11:17:34 +0500

    class practice...

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
-public class Main {
+// import java.util.*;
+import java.util.Scanner;  // Import the Scanner class
+
+class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter your Name: ");
+        String name = myObj.nextLine();
+        System.out.println("Your Name is " + name + ".");
+        myObj.close();
     }
 }
--- OOP/src/App.java
+++ OOP/src/App.java
@@ -1,5 +1,25 @@
+// import java.util.*;
+import java.util.Scanner;  // Import the Scanner class
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Noice");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int firstNum = scan.nextInt();
+        System.out.println("Enter second number: ");
+        int secondNum = scan.nextInt();
+        System.out.println("Enter function to perform: ");
+        char function = scan.next().charAt(0);  
+        scan.close();
+        
+        int result = switch (function) {
+            case '+' -> firstNum + secondNum;
+            case '-' -> firstNum - secondNum;
+            case '/' -> firstNum / secondNum;
+            case '*' -> firstNum * secondNum;
+            default -> 0;
+        };
+
+        System.out.println("The result is " + result);
     }
 }
